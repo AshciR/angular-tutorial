6d1d71178cc80b54e0b98844022a9bce11b92ba5
Richard Walker <richard.walker.90@gmail.com>
2020-03-23 19:20:19 -0400

    Finish lesson 13, ngSwitch

--- src/app/struct-directives/struct-directives.component.ts
+++ src/app/struct-directives/struct-directives.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class StructDirectivesComponent implements OnInit {
 
   public displayName: boolean = false;
-
+  public color: String = "cyan";
   constructor() { }
 
   ngOnInit() {
